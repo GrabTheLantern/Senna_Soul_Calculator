de24a9662fdce0de00197affb659574ccfc82878
GrabTheLantern <pasinatoj@gmail.com>
2022-01-12 21:19:06 -0700

    Update Senna_Soul_Calc.py
    Changed multiple prints to just one variable that gets added on to. Thanks to /u/vladc92.

--- Senna_Soul_Calc.py
+++ Senna_Soul_Calc.py
@@ -2,6 +2,7 @@
 
 souls=int(input('How many souls do you have? ' ))
 souldmg = souls * .75
+bonus = 'With ' + str(souls) + ' souls, you have gain the following bonuses: \n' + str(souldmg) + ' damage.\n'
 if souls >= 20:
     soulmult = souls // 20
     soulrange = int(soulmult * 20)
@@ -9,8 +10,10 @@ if souls >= 20:
     if soulcrit >= 100:
         lifesteal = (soulcrit -100) * .35
         soulcrit = 100
-        print('With ' + str(souls) + ' souls, you have gain the following bonuses:\n' + str(souldmg)+' damage\n'+ str(soulrange) + ' range\n' + str(soulcrit) + ' crit chance\n' + str(lifesteal)+ ' lifesteal \n')
+        bonus= bonus + str(soulrange) + ' range \n' + str(soulcrit) + ' crit chance\n' + str(lifesteal)+ ' lifesteal \n'
+        print(bonus)
     else:
-        print('With ' + str(souls) + ' souls, you have gain the following bonuses:\n' + str(souldmg)+' damage\n'+ str(soulrange) + ' range\n' + str(soulcrit) + ' crit chance\n')
+        bonus = bonus + str(soulrange) + ' range \n' + str(soulcrit) + ' crit \n'
+        print(bonus)
 else:
-    print('With ' + str(souls) + ' souls, you have an additional ' + str(souldmg) + ' damage.')
+    print(bonus)
